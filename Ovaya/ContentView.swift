661dff8e82fc5a4dc0896c83a45503d879ab65ae
yashsomkuwar <yash@getswipe.in>
2024-08-22 16:35:47 +0530

    Initial Commit

---
+++ Ovaya/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ovaya
+//
+//  Created by Yash Somkuwar on 22/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ovaya/OvayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OvayaApp.swift
+//  Ovaya
+//
+//  Created by Yash Somkuwar on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct OvayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
